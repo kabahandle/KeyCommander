848418c147dc43169c9bf5a7c6012f806ccb5786
teruyoshi_kaibara_2016 <teruyoshi_kaibara_2016@la-bit.sakura.ne.jp>
2020-02-16 14:15:00 +0900

    - コマンドラインの引数をshiftを使って、10個以上に対応。

--- KeyCommander/App_Data/dobat.bat
+++ KeyCommander/App_Data/dobat.bat
@@ -1,8 +1,19 @@
 @echo off
 cd /D %1
+
 shift
-set pg=%1
+
+:check
+if "%1"=="" goto final
+if "%1"=="""" goto final
+set pg=%pg% %1
 shift
+goto check
+:final
+
+rem set pg=%1
+rem shift
+
 %pg% %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 
