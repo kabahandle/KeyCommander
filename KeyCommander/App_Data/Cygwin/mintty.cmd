b7de3ce5e86ed985cc977762c51b3e9f4f2880a4
kaba30 <teruyoshi_kaibara_2016@la-bit.sakura.ne.jp>
2020-03-21 13:03:37 +0900

    - CygWinの1行コマンド実行用MinTty用cmdと、DosBoxの1行コマンド実行用cmdのコードのバグを修正。

--- KeyCommander/App_Data/Cygwin/mintty.cmd
+++ KeyCommander/App_Data/Cygwin/mintty.cmd
@@ -1,7 +1,7 @@
 @echo off
 cd /d %1
 shift
-%cygwinbinpath%\mintty.exe -h always 
+%cygwinbinpath%\mintty.exe -h always -e %cygwinbinpath%\bash -i -t -c "%1 %2 %3 %4 %5 %6 %7 %8 %9"
 rem %cygwinbinpath%\mintty.exe -h always -e %cygwinbinpath%\bash  --login -i -c "cd %1"
 rem %cygwinbinpath%\mintty.exe -h always -e %cygwinbinpath%\bash --login -i -t -c "%1 %2 %3 %4 %5 %6 %7 %8 %9"
 
--- KeyCommander/App_Data/dosbox/exec.cmd
+++ KeyCommander/App_Data/dosbox/exec.cmd
@@ -1,5 +1,6 @@
+set d=%1
 set pg="%dosboxbindir%\exec.bat" %1 %2 %3 %4 %5 %6 %7 %8 %9
-rem c:\dos220\dosbox.exe -noautoexec -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "c:"  -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64-3" -c "c:"  -c "%pg%"
-rem c:\dos220\dosbox.exe  -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "cd c:\dos220" -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64-3" -c "c:"  -c "%pg%"
-%dosboxbindir%\dosbox.exe  -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "cd c:\dos220" -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64-3" -c "c:"  -c "%pg%"
+rem c:\dos220\dosbox.exe -noautoexec -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "c:"  -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64" -c "c:"  -c "%pg%"
+rem c:\dos220\dosbox.exe  -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "cd c:\dos220" -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64" -c "c:"  -c "%pg%"
+%dosboxbindir%\dosbox.exe  -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "cd c:\dos220" -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64" -c "cd /d %d%"  -c "%pg%"
 
