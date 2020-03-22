52babea1f1849dbf18d9e3f6b94dc3f4fd0fe5c4
kaba30 <teruyoshi_kaibara_2016@la-bit.sakura.ne.jp>
2020-03-23 06:15:58 +0900

    - DosBox 用に、cd /d <フォルダ> と同じ動作をするよう、exec.cmd を修正。

--- KeyCommander/App_Data/dosbox/exec.cmd
+++ KeyCommander/App_Data/dosbox/exec.cmd
@@ -1,6 +1,9 @@
 set d=%1
-set pg="%dosboxbindir%\exec.bat" %1 %2 %3 %4 %5 %6 %7 %8 %9
-rem c:\dos220\dosbox.exe -noautoexec -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "c:"  -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64" -c "c:"  -c "%pg%"
-rem c:\dos220\dosbox.exe  -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "cd c:\dos220" -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64" -c "c:"  -c "%pg%"
-%dosboxbindir%\dosbox.exe  -conf "c:\dos220\dosbox.conf" -c "mount c c:\\"  -c "cd c:\dos220" -c "c:\dos220\chej\chej jp" -c "mount d c:\cygwin64" -c "cd /d %d%"  -c "%pg%"
+rem RefURL:  http://simple.hatenablog.jp/entry/BAT/002
+set drv=%~d1
+shift
 
+set pg="c:\dos220\exec.bat" %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem D:\220again_dosbox\dos220\dosbox.exe -c "mount c d:\220again_dosbox" -c "mount d d:\\" -c "c:"  -c "%pg%
+rem c:\220again\dos220\dosbox.exe -noautoexec -conf "c:\220again\dos220\dosbox.conf" -c "mount c c:\220again" -c "c:\dos220\chej\chej jp" -c "mount d c:\\cygwin-3" -c "c:"  -c "%pg%"
+c:\220again\dos220\dosbox.exe -conf "c:\\220again\\dos220\\dosbox.conf" -c "mount c c:\\220again" -c "c:\\dos220\\chej\\chej jp" -c "mount d m:\\cygwin64" -c "mount m m:\\ " -c " %drv% " -c "cd %d% " -c "%pg%"
