dde832f5d04c22d45e4ca994be26bbe8fd3a3338
kaba30 <teruyoshi_kaibara_2016@la-bit.sakura.ne.jp>
2020-03-25 07:21:29 +0900

    - DosBox用の exec.cmd を、自分の環境用に少し変更。

--- KeyCommander/App_Data/dosbox/exec.cmd
+++ KeyCommander/App_Data/dosbox/exec.cmd
@@ -9,4 +9,4 @@ shift
 set pg="c:\dos220\exec.bat" %1 %2 %3 %4 %5 %6 %7 %8 %9
 rem D:\220again_dosbox\dos220\dosbox.exe -c "mount c d:\220again_dosbox" -c "mount d d:\\" -c "c:"  -c "%pg%
 rem c:\220again\dos220\dosbox.exe -noautoexec -conf "c:\220again\dos220\dosbox.conf" -c "mount c c:\220again" -c "c:\dos220\chej\chej jp" -c "mount d c:\\cygwin-3" -c "c:"  -c "%pg%"
-c:\220again\dos220\dosbox.exe -conf "c:\\220again\\dos220\\dosbox.conf" -c "mount c c:\\220again" -c "c:\\dos220\\chej\\chej jp" -c "mount d m:\\cygwin64" -c "mount m m:\\ " -c "mount w w:\\ " -c " %drv% " -c "cd %d% " -c "%pg%"
+c:\220again\dos220\dosbox.exe -conf "c:\\220again\\dos220\\dosbox.conf" -c "mount c c:\\220again" -c "c:\\dos220\\chej\\chej jp" -c "mount d m:\\cygwin64" -c "mount m m:\\ " -c "mount g g:\\ " -c " %drv% " -c "cd %d% " -c "%pg%"
