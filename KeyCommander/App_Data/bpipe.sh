3f183953610ea4deb2227aeb2b74c985b8da7b58
teruyoshi_kaibara_2016 <teruyoshi_kaibara_2016@la-bit.sakura.ne.jp>
2020-05-24 11:35:52 +0900

    - test commit.

---
+++ KeyCommander/App_Data/bpipe.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/bash
+CMDLINE=""
+while [ "$1" != "" ]
+do
+  if [ "$1" = ":" ] ; then
+      CMDLINE="${CMDLINE} | "
+  elif [ "$1" = ":[" ] ; then
+    CMDLINE="${CMDLINE} < "
+  elif [ "$1" = ":]" ] ; then 
+        CMDLINE="${CMDLINE} > "
+  else
+      CMDLINE="${CMDLINE} $1"
+  fi
+  shift
+done
+
+#bash.exe -c "\"${CMDLINE}"\"
+#echo bash.exe -c "\"${CMDLINE}"\"
+cmd /c "${CMDLINE}"
+echo cmd /c "${CMDLINE}"
